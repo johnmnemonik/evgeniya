2bb0293178470f2033747b4e05f513c90282624e
johnmnemonik.jm <johnmnemonik.jm@gmail.com>
2019-08-18 19:17:51 +0300

    info

--- api/views.py
+++ api/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import JsonResponse
-from django.db.models import Q
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
